6599907473759d1e0d1dcd8981c41a5f86686d21
Atsushi1216 <ats1216@gmail.com>
2021-12-27 12:16:08 +0000

    [Add]devise機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :admin do
-
-  end
-
-  #管理者
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-  }
-  #顧客用
-  devise_for :customers,skip: [:passwords], controllers: {
-    registrations: "customer/registrations",
-    sessions: 'customer/sessions'
-  }
 
   # 顧客側設定
     root "homes#top"
