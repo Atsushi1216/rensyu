53a8d3906aa0d5fe4066ce94f0298b5261887087
Atsushi1216 <ats1216@gmail.com>
2021-12-28 12:17:29 +0000

    [fix]top

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # 顧客側設定
     root "homes#top"
     get "about" => "homes#about"
 
